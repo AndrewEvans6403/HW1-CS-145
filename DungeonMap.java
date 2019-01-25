b718aaa4092287b3413d8baa55bf1bf188261dc7
Nils <nilsrys@live.com>
2019-01-25 15:03:56 -0800

    Update DungeonMap.java

--- DungeonMap.java
+++ DungeonMap.java
@@ -1,2 +1,17 @@
 public class DungeonMap {
+  private String dungeonMap[][] = {{"*","*","*","*","*"},{"*","*","*","*","*"},{"*","*","*","*","*"},{"*","*","*","*","*"},{"*","*","*","*","*"}};
+
+  
+  public void dungeon(){
+        System.out.println("------------");
+        for (int y = 0; y < 5; y++){
+            System.out.print("|");
+            for (int x = 0; x < 5; x++ ){
+                System.out.print(dungeonMap[y][x]);
+                System.out.print(" ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("------------");
+    }
 }
