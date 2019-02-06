42a5b1ac21e2b485ba04ed5aff884947b4749942
Nils <nilsrys@live.com>
2019-02-06 14:25:20 -0800

    Update DungeonMap.java

--- DungeonMap.java
+++ DungeonMap.java
@@ -26,3 +26,86 @@ public class DungeonMap {
         System.out.println("------------");
     }
 }
+
+
+
+    public int xPos, yPos;
+    public double userGold = 0;
+    public int health = 0;
+    public int damage = 0;
+    public double goldMultiplier = 1;
+    private static int ORC_DAMAGE = 6, ORC_INIT_HEALTH = 5, ORC_GOLD = 2;
+    private String MAP_WALL = "*";
+    private String MAP_FLOOR = " ";
+    private String MAP_EXPLORED = "o";
+    private String MAP_PLAYER = "P";
+    public String dungeonMap[][];
+
+
+
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input maps width");
+        int x = scanner.nextInt();
+        System.out.println("input maps height");
+        int y = scanner.nextInt();
+        main.setDungeonMap(x, y);
+        main.printDungeonMap();
+//        String classType = main.setClass();
+//        System.out.println(classType);
+
+
+    }
+
+
+
+    public String setClass() {
+
+        boolean classSelect = true;
+        String occupation = "";
+        while (classSelect) {
+            System.out.println("Select your class: (T)heif or (W)arrior");
+            Scanner scanner = new Scanner(System.in);
+            occupation = scanner.nextLine();
+            if (occupation.equalsIgnoreCase("t")) {
+                health = 70;
+                damage = 10;
+                goldMultiplier = 1.2;
+                classSelect = false;
+            } else if (occupation.equalsIgnoreCase("w")) {
+                health = 100;
+                damage = 15;
+                goldMultiplier = 1;
+                classSelect = false;
+            } else {
+                System.out.println("Class failed to select please try again.");
+            }
+        }
+        return occupation;
+    }
+
+    public void setDungeonMap(int xPos, int yPos){
+        dungeonMap = new String[yPos][xPos];
+        for (int i = 0; i < yPos; i++){
+            for (int j = 0; j < xPos; j++){
+                if (j == 0 || j == xPos-1 || i == 0 || i == yPos-1){
+                    dungeonMap[i][j] = MAP_WALL;
+                } else{
+                    dungeonMap[i][j] = MAP_FLOOR;
+                }
+            }
+        }
+
+    }
+
+    public void printDungeonMap(){
+        for (int i = 0; i < dungeonMap.length; i++){
+            for (int j = 0; j < dungeonMap[i].length; j++){
+                System.out.print(dungeonMap[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
