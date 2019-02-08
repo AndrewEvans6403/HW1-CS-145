7642e04a75bd297d83f34a99e52e7f2cc02b20b8
AndrewEvans6403 <andrew.evans.6403@hotmail.com>
2019-02-08 15:03:02 -0800

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,8 +1,13 @@
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class Room {
-    private boolean encounter;
+    private boolean visited = false;
+
+    private String mapIcon;
+
+
 
     public void enter(Player player) {
         Random random = new Random();
@@ -10,77 +15,84 @@ public class Room {
 
         int encounter = random.nextInt(2);
 
-        // if encounter is monster
-        if (encounter == 0) {
-
-            Monster monster = new Monster();
-            monster.initializeMonster();
-
-            System.out.println("A " + monster.getMonsterType() + " appears");
-
-            while (monster.getHealth() > 0) {
-                System.out.println("Select an action: [1] Attack, [2] Run ");
-                int combatAction = scanner.nextInt();
+        if (!visited) {
 
 
-                //attack
-                if (combatAction == 1) {
-                    //monster attack
-                    monster.attack(player);
-                    System.out.println("The " + monster.getMonsterType() + "hits you for " + monster.getDamage() + " damage!");
+            // if encounter is monster
+            if (encounter == 0) {
 
-                    //player attack
-                    player.attack(monster);
-                    System.out.println("You attack and hit the " + monster.getMonsterType() + " for " + player.getDamage() + " damage");
-                    if (monster.getHealth() <= 0) {
-                        System.out.println("The " + monster.getMonsterType() + " dies!");
+                Monster monster = new Monster();
+                monster.initializeMonster();
+
+                System.out.println("A " + monster.getMonsterType() + " appears");
+
+                while (monster.getHealth() > 0) {
+                    System.out.println("Select an action: [1] Attack, [2] Run ");
+                    int combatAction = scanner.nextInt();
+
+
+                    //attack
+                    if (combatAction == 1) {
+                        //monster attack
+                        monster.attack(player);
+                        System.out.println("The " + monster.getMonsterType() + "hits you for " + monster.getDamage() + " damage!");
+
+                        //player attack
+                        player.attack(monster);
+                        System.out.println("You attack and hit the " + monster.getMonsterType() + " for " + player.getDamage() + " damage");
+                        if (monster.getHealth() <= 0) {
+                            System.out.println("The " + monster.getMonsterType() + " dies!");
+
+                        }
+                    }
+
+                    //run
+                    else if (combatAction == 2) {
+                        monster.attack(player);
+                        System.out.println("You try to run... ");
+                        System.out.println("The " + monster.getMonsterType() + "attacks and hits you for " + monster.getDamage() + " damage as you try to escape");
+                        break;
+
+                    } else {
+                        System.out.println("Invalid input");
 
                     }
-                }
 
-                //run
-                else if (combatAction == 2) {
-                    monster.attack(player);
-                    System.out.println("You try to run... ");
-                    System.out.println("The " + monster.getMonsterType() + "attacks and hits you for " + monster.getDamage() + " damage as you try to escape");
-                    break;
 
                 }
+                visited = true;
 
-                else {
-                    System.out.println("Invalid input");
+            }
 
+            // if encounter is loot
+            else if (encounter == 1) {
+                int goldOrHeal = random.nextInt(2);
+
+                // health encounter
+                if (goldOrHeal == 0) {
+                    int healthPotion = random.nextInt(30);
+                    player.onHeal(healthPotion);
                 }
 
+                // gold encounter
+                if (goldOrHeal == 1) {
+                    int goldLoot = random.nextInt(30);
+                    player.onLoot(goldLoot);
 
+
+                }
             }
 
         }
-
-        // if encounter is loot
-        else if (encounter == 1) {
-            int goldOrHeal = random.nextInt(2);
-
-            // health encounter
-            if (goldOrHeal == 0){
-                int healthPotion = random.nextInt(30);
-                player.onHeal(healthPotion);
-            }
-
-            // gold encounter
-            if(goldOrHeal == 1){
-                int goldLoot = random.nextInt(30);
-                player.onLoot(goldLoot);
-
-
-
-            }
-        }
-
     }
 
 
-    public void setEncounter(boolean encounter) {
-        this.encounter = encounter;
+
+    public String getMapIcon() {
+        return mapIcon;
+    }
+
+    public void setMapIcon(String mapIcon) {
+        this.mapIcon = mapIcon;
     }
 }
